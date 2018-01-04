c3bfe28e3c4f51b5cbbd0471382f51636c7a62a8
Andrew Usher <andrewusher00@gmail.com>
2018-01-04 11:04:47 -0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const leftDiv = document.querySelector('.left');
+const rightDiv = document.querySelector('.right');
+const container = document.querySelector('.container');
+
+leftDiv.addEventListener('mouseenter', () => {
+  container.classList.add('hover-left');
+});
+
+leftDiv.addEventListener('mouseleave', () => {
+  container.classList.remove('hover-left');
+});
+
+rightDiv.addEventListener('mouseenter', () => {
+  container.classList.add('hover-right');
+});
+
+rightDiv.addEventListener('mouseleave', () => {
+  container.classList.remove('hover-right');
+});
