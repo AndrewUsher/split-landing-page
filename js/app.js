be876e367abc72855752cc9d514473ae5d758fff
Andrew Usher <andrewusher00@gmail.com>
2018-11-17 19:55:25 -0600

    Change spacing type :)

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
-const leftDiv = document.querySelector('.left');
-const rightDiv = document.querySelector('.right');
-const container = document.querySelector('.container');
+const leftDiv = document.querySelector(".left");
+const rightDiv = document.querySelector(".right");
+const container = document.querySelector(".container");
 
-leftDiv.addEventListener('mouseenter', () => {
-  container.classList.add('hover-left');
+leftDiv.addEventListener("mouseenter", () => {
+  container.classList.add("hover-left");
 });
 
-leftDiv.addEventListener('mouseleave', () => {
-  container.classList.remove('hover-left');
+leftDiv.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-left");
 });
 
-rightDiv.addEventListener('mouseenter', () => {
-  container.classList.add('hover-right');
+rightDiv.addEventListener("mouseenter", () => {
+  container.classList.add("hover-right");
 });
 
-rightDiv.addEventListener('mouseleave', () => {
-  container.classList.remove('hover-right');
+rightDiv.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-right");
 });
